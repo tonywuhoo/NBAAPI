3b8adb611ce35bc127c17cd0be004b52df3e18f6
Tony Wu <tonywu@Tonys-MacBook-Air-2.local>
2022-10-31 13:03:33 -0400

    update

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import db from "./db/connection.js"
 import routes from "./routes/index.js"
 import doShit from "./seed/data.js"
 
-doShit()
-
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -16,6 +14,7 @@ app.use(cors());
 app.use(logger("dev"));
 
 app.use("/", routes);
+
 
 db.on("connected", () => {
   console.clear();
